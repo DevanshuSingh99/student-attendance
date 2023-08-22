276656dadd9b57ae341b0828b8fb6339898a8ecc
DevanshuSingh99 <65912470+DevanshuSingh99@users.noreply.github.com>
2023-08-23 01:23:49 +0530

    [Backend] Get attendance API

--- backend/controllers/attendanceController.js
+++ backend/controllers/attendanceController.js
@@ -6,17 +6,32 @@ attendanceController.markAttendance = async (req, res) => {
     try {
         var { userId, subjects, date } = req.body;
         if (!userId || !date) return res.status(400).send("Missing parameters!");
-        console.log(userId, subjects, date);
-        var formatedDate = new Date(date).toISOString().split("T")[0];
-        console.log(formatedDate);
+        var formatedDate = new Date(date);
+        formatedDate.setDate(formatedDate.getDate() + 1);
+        formatedDate = formatedDate.toISOString().split("T")[0];
         var attendanceRecord = await attendanceSchema.findOne({ date: formatedDate });
         if (attendanceRecord) {
-            await attendanceSchema.findByIdAndUpdate(attendanceRecord["_id"], {[`students.${userId}`]:subjects});
+            await attendanceSchema.findByIdAndUpdate(attendanceRecord["_id"], { [`students.${userId}`]: subjects });
         } else {
-            await attendanceSchema.create({ date:formatedDate, students: { [userId]: subjects } });
+            await attendanceSchema.create({ date: formatedDate, students: { [userId]: subjects } });
         }
 
         res.send("Attendance Marked");
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Something went wrong!");
+    }
+};
+
+attendanceController.getAttendance = async (req, res) => {
+    try {
+        var { date } = req.query;
+        if (!date) return res.status(400).send("Missing parameters!");
+        var dateTime = new Date(date);
+        dateTime.setDate(dateTime.getDate() + 1);
+        var attendance = await attendanceSchema.findOne({ date: dateTime.toISOString().split("T")[0] });
+        console.log(attendance.students[req.userDetails["_id"]]);
+        res.send(attendance ? (attendance.students[req.userDetails["_id"]] ? attendance.students[req.userDetails["_id"]] : []) : "Attendance not marked yet");
     } catch (error) {
         console.log(error);
         res.status(400).send("Something went wrong!");
--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -5,5 +5,6 @@ var adminMiddleware = require("../middlewares/adminMiddleware");
 
 attendanceRoutes.use(authMiddleware)
 attendanceRoutes.route("/markAttendance").post(adminMiddleware,attendanceController.markAttendance);
+attendanceRoutes.route("/getAttendance").get(attendanceController.getAttendance);
 
 module.exports = attendanceRoutes;
