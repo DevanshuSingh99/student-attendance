9bb3717ad4884bad9339b64b7869603ced2eba71
DevanshuSingh99 <65912470+DevanshuSingh99@users.noreply.github.com>
2023-08-23 01:24:33 +0530

    [Frontend] - Navbar Data  - logout

---
+++ frontend/src/Components/Navbar/Navbardata.js
@@ -0,0 +1,27 @@
+export const NavbarData = [
+    {
+        title: "Student Database",
+        path: "/dashboard/studentdatabase",
+        roles: ["Admin"],
+    },
+    {
+        title: "Student Attendance",
+        path: "/dashboard/studentattendance",
+        roles: ["Admin"],
+    },
+    {
+        title: "Profile",
+        path: "/dashboard/myprofile",
+        roles: ["Student"],
+    },
+    {
+        title: "Attendance",
+        path: "/dashboard/myattendance",
+        roles: ["Student"],
+    },
+    {
+        title: "Logout",
+        path: "/",
+        roles: ["Student","Admin"],
+    },
+];
